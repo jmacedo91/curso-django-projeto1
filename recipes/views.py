9a248e69d51c5d6407ed79cdbf05ea5c9d61f312
jmacedo91 <jonathan.silvamacedo@gmail.com>
2024-12-05 19:09:36 -0300

    Criando templates

--- recipes/views.py
+++ recipes/views.py
@@ -1,12 +1,11 @@
 from django.http import HttpResponse
-
-# from django.shortcuts import render
+from django.shortcuts import render
 
 # Create your views here.
 
 
 def home(request):
-    return HttpResponse('Home Page')
+    return render(request, 'recipes/home.html')
 
 
 def contato(request):
